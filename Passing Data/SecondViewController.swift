0ccc9f9e87d9e5fbf7618c8bd0b38a611e5969ca
Harpreet Kaur <harpreetvirk902@gmail.com>
2019-03-26 15:29:36 -0400

    Passing data between view controllers

---
+++ Passing Data/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  Passing Data
+//
+//  Created by Happy on 25/03/19.
+//  Copyright © 2019 Happy. All rights reserved.
+//
+import UIKit
+protocol CanReceive {
+    func dataReceived(data: String)
+
+}
+
+class SecondViewController: UIViewController {
+    var delegate : CanReceive?
+    var data = ""
+ @IBOutlet weak var Label: UILabel!
+    
+    @IBOutlet weak var TextField: UITextField!
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        Label.text = data
+    }
+    @IBAction func Back(_ sender: UIButton) {
+     delegate?.dataReceived(data: TextField.text!)
+        dismiss(animated: true, completion: nil)
+    }
+
+}
--- Passing Data/ViewController.swift
+++ Passing Data/ViewController.swift
@@ -8,13 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,CanReceive{
 
+    @IBOutlet weak var Label1: UILabel!
+    @IBOutlet weak var TextField1: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+   
     }
-
-
+    @IBAction func ChangeBlue(_ sender: Any) {
+        view.backgroundColor = UIColor.brown
+    }
+    @IBAction func SEND(_ sender: UIButton) {
+    performSegue(withIdentifier: "SEND", sender:self)
+        }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "SEND"{
+        let SecondVC = segue.destination as! SecondViewController
+        SecondVC.data = TextField1.text!
+            SecondVC.delegate = self
+        
+        }
+        
+    }
+    func dataReceived(data:String)  {
+         Label1.text = data
+    }
 }
 
