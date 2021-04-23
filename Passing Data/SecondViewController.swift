ae6852163d2ea2d73cda9868bd179de5eaa5360c
Harpreet <harpreetvirk902@gmail.com>
2021-04-23 15:51:18 -0400

    implementing alert box

--- Passing Data/SecondViewController.swift
+++ Passing Data/SecondViewController.swift
@@ -8,26 +8,33 @@
 import UIKit
 protocol CanReceive {
     func dataReceived(data: String)
-
+    
 }
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController,UITextFieldDelegate {
     var delegate : CanReceive?
     var data = ""
- @IBOutlet weak var Label: UILabel!
+    @IBOutlet weak var Label: UILabel!
     
     @IBOutlet weak var TextField: UITextField!
     override func viewDidLoad() {
         
         super.viewDidLoad()
         Label.text = data
+        TextField.delegate = self
     }
     @IBAction func Back(_ sender: UIButton) {
-     delegate?.dataReceived(data: TextField.text!)
-        dismiss(animated: true, completion: nil)
-    }
-    func textFieldDidBeginEditing(textField: UITextField) {
+        if TextField.text == "" {
+            let alert = UIAlertController(title:"Attention", message: "please write something to send another view controller", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        } else {
+            delegate?.dataReceived(data: TextField.text!)
+            dismiss(animated: true, completion: nil)
+        }}
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
         TextField.text = ""
     }
-
+    
 }
--- Passing Data/ViewController.swift
+++ Passing Data/ViewController.swift
@@ -8,32 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController ,CanReceive{
-
+class ViewController: UIViewController,CanReceive, UITextFieldDelegate{
+    
     @IBOutlet weak var Label1: UILabel!
     @IBOutlet weak var TextField1: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-   
+        TextField1.delegate = self
     }
-    func textFieldDidBeginEditing(textField: UITextField) {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
         TextField1.text = ""
     }
     @IBAction func SEND(_ sender: UIButton) {
-    performSegue(withIdentifier: "SEND", sender:self)
-        }
+        if TextField1.text == "" {
+            let alert = UIAlertController(title:"Attention", message: "please write something to send another view controller", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        } else {
+            performSegue(withIdentifier: "SEND", sender:self)
+        }}
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "SEND"{
-        let SecondVC = segue.destination as! SecondViewController
-        SecondVC.data = TextField1.text!
+            let SecondVC = segue.destination as! SecondViewController
+            SecondVC.data = TextField1.text!
             SecondVC.delegate = self
-        
+            
         }
         
     }
     func dataReceived(data:String)  {
-         Label1.text = data
+        Label1.text = data
     }
 }
 
